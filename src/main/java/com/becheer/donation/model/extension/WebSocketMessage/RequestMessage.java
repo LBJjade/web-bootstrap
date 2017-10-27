087a0395dffbb352fb1759b747ce297502e40076
libingjian <lbj>
2017-10-27 18:30:20 +0800

    feat: 尝试websocket

---
+++ src/main/java/com/becheer/donation/model/extension/WebSocketMessage/RequestMessage.java
@@ -0,0 +1,9 @@
+package com.becheer.donation.model.extension.WebSocketMessage;
+
+public class RequestMessage {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/com/becheer/donation/model/extension/WebSocketMessage/ResponseMessage.java
@@ -0,0 +1,13 @@
+package com.becheer.donation.model.extension.WebSocketMessage;
+
+public class ResponseMessage {
+    private String responseMessage;
+
+    public ResponseMessage(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+}
