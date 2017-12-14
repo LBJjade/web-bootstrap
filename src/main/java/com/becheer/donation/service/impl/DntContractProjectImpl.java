192d812aee1aebb1e96da8bd5fc03af4f81eec1f
libingjian <lbj>
2017-12-14 09:48:04 +0800

    捐赠意向提交接口调整 issue #46

---
+++ src/main/java/com/becheer/donation/service/impl/DntContractProjectImpl.java
@@ -0,0 +1,29 @@
+package com.becheer.donation.service.impl;
+
+import com.becheer.donation.dao.DntContractProjectMapper;
+import com.becheer.donation.model.DntContractProject;
+import com.becheer.donation.service.IDntContractProjectService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * 包名: com.becheer.donation.service.impl
+ * 文件说明: 描述当文件的用途
+ * 创建人:LBJ
+ * 创建日期: 2017/12/12 17:14
+ * 版本:V1.0
+ * Copyright  2017 广州品清科技有限公司
+ */
+@Service
+public class DntContractProjectImpl implements IDntContractProjectService {
+
+    @Resource
+    DntContractProjectMapper dntContractProjectMapper;
+
+    @Override
+    public List<DntContractProject> selectProjectIdBycontraId(Integer id) {
+        return dntContractProjectMapper.selectProjectIdBycontraId(id);
+    }
+}
