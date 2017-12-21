3cd32234ec49eab6162291b3873d507d8af7813e
xiaokepu <xiaokepu@becheer.com>
2017-12-21 16:13:10 +0800

    添加@Component注解

--- src/main/java/com/becheer/donation/dao/AreaMapper.java
+++ src/main/java/com/becheer/donation/dao/AreaMapper.java
@@ -1,18 +1,21 @@
 package com.becheer.donation.dao;
 
 import com.becheer.donation.model.Area;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
 
 /**
  * 包名: com.becheer.donation.dao
- * 文件说明: 描述当文件的用途
+ * 文件说明: 省市区三级联动Mapper
  * 创建人:amber
  * 创建日期: 2017/12/12 16:46
  * 版本:V1.0
  * Copyright © 2017 广州品清科技有限公司
  */
+
+@Component
 public interface AreaMapper {
     List<Area> selectAreaByParentId(long parentId);
 
--- src/main/java/com/becheer/donation/dao/ProgressMapper.java
+++ src/main/java/com/becheer/donation/dao/ProgressMapper.java
@@ -2,6 +2,7 @@ package com.becheer.donation.dao;
 
 import com.becheer.donation.model.Progress;
 import com.becheer.donation.model.extension.progress.ProgressExtension;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
@@ -10,6 +11,7 @@ import java.util.List;
 * Creator : xiaokepu
 * Date : 2017-10-07
 */
+@Component
 public interface ProgressMapper {
     List<ProgressExtension>SelectAllProgress(long refRecordId,String refTable);
 
